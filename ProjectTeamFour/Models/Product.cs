699a23992876339c0c78606a81854f89f431f4e8
sonia1013 <soniatoyota5941@gmail.com>
2021-02-01 18:46:01 +0800

    0201 init by Sonia1013

---
+++ ProjectTeamFour/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTeamFour.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brief { get; set; }
+        public string Category { get; set; }
+        public string Status { get; set; }
+        public decimal GoalAmount { get; set; }
+        public decimal FundedAnount { get; set; }
+        public int Progress{ get; set; }
+        public DateTime DeadLine { get; set; }
+    }
+}
