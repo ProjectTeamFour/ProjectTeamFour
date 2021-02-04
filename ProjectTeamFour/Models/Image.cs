ebedecbfc1af36245edd7340b4f3bdc41bccb6f6
WangChunYu <4a037902@gmail.com>
2021-02-04 18:25:27 +0800

    projectCRUD_APP_Code_ImagePartialView_胖羽_02_04

---
+++ ProjectTeamFour/Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectTeamFour.Models
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string ImageUrl { get; set; }
+        public int ProjectId { get; set; }
+        [ForeignKey("ProjectId")]
+        public virtual Project Project { get; set; }
+
+    }
+}
