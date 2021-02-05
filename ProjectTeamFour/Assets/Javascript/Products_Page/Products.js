759feb193b263807d30e3d9ef253075b4f835981
Kane Kai <kkbbg3106@gmail.com>
2021-02-05 16:16:21 +0800

    重新 上傳 products view

---
+++ ProjectTeamFour/Assets/Javascript/Products_Page/Products.js
@@ -0,0 +1,9 @@
+﻿$('.category-group').slick({
+    centerMode: false,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplaySpeed: 2000,
+    arrows: false,
+    draggable: true,
+    focusOnSelect: true
+});
