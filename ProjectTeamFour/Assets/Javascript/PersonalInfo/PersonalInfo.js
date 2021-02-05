2babda9dd72a7cfb88a91abc8879a8ade0e8f353
Kane Kai <kkbbg3106@gmail.com>
2021-02-05 15:45:50 +0800

    上傳 個人資料頁(待改) 等等回家調整 0205

---
+++ ProjectTeamFour/Assets/Javascript/PersonalInfo/PersonalInfo.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('.autoplay').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        arrows: true,
+        accessibility: true,
+        prevArrow: $(".prev"),
+        nextArrow: $(".next"),
+    });
+    $('#dismiss, .overlay').on('click', function () {
+        $('#sidebar').removeClass('active');
+        $('.overlay').removeClass('active');
+    });
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').addClass('active');
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
