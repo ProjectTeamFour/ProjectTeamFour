32fb124eeb4ce96f3c711659e87882769ff511ff
sonia1013 <soniatoyota5941@gmail.com>
2021-02-05 16:46:34 +0800

    merge and Revert "0205 merge files"

---
+++ ProjectTeamFour/Assets/Javascript/PaymentPage/pay.js
@@ -0,0 +1,41 @@
+﻿let credit_one = document.getElementById("credit-card");
+let credit_split = document.getElementById("credit-card-split");
+let atm = document.getElementById("credit-atm");
+let market = document.getElementById("credit-market");
+
+let sp_one = document.getElementById("one");
+let sp_split = document.getElementById("split");
+let sp_atm = document.getElementById("atm");
+let sp_market = document.getElementById("market");
+
+
+
+
+credit_one.onclick = function () {
+    sp_one.classList.add("widthOfblock");
+    sp_split.classList.remove("widthOfblock");
+    sp_atm.classList.remove("widthOfblock");
+    sp_market.classList.remove("widthOfblock");
+}
+
+credit_split.onclick = function () {
+    sp_split.classList.add("widthOfblock");
+    sp_one.classList.remove("widthOfblock");
+    sp_atm.classList.remove("widthOfblock");
+    sp_market.classList.remove("widthOfblock");
+}
+
+atm.onclick = function () {
+    sp_split.classList.remove("widthOfblock");
+    sp_one.classList.remove("widthOfblock");
+    sp_atm.classList.add("widthOfblock");
+    sp_market.classList.remove("widthOfblock");
+}
+
+market.onclick = function () {
+    sp_split.classList.remove("widthOfblock");
+    sp_one.classList.remove("widthOfblock");
+    sp_atm.classList.remove("widthOfblock");
+    sp_market.classList.add("widthOfblock");
+}
+
