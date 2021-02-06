b9b8b75f1f319ffb74840619da5e3eaad2b891bc
sonia1013 <soniatoyota5941@gmail.com>
2021-02-06 17:02:15 +0800

    刪除 - 個人頁部分 view

--- ProjectTeamFour/Controllers/ProductDetailController.cs
+++ ProjectTeamFour/Controllers/ProductDetailController.cs
@@ -9,14 +9,6 @@ namespace ProjectTeamFour.Controllers
 {
     public class ProductDetailController : Controller
     {
-        protected List<ProductDetail> productDetails = new List<ProductDetail>
-        {
-            new ProductDetail{ Id=1, ProductName="開啟與孩子的第一場程式冒險 《解碼傳說 CO·DECODE》",CreatorName="Papacode-程式老爹",Category="遊戲出版",FundedPeople=828,GoalAmount=140113,
-                                FundedAmount=1609173},
-            new ProductDetail{ Id=2,ProductName="Cheerpod 智慧滑鼠 | 一片在手,螢幕隨控",CreatorName="cheerdots TW",Category="科技設計",FundedPeople=5,GoalAmount=10000000},
-
-        };
-
 
         // GET: ProductDetail
         public ActionResult Index()
