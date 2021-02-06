d90855be654aaf9f0dfc06a635cad9db193eafca
WangChunYu <4a037902@gmail.com>
2021-02-06 10:22:28 +0800

    登入系統 加入api

---
+++ ProjectTeamFour/Service/MemberService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjectTeamFour.Reposities;
+using ProjectTeamFour.Models;
+using System.Data.Entity;
+using System.Net.Http;
+using ProjectTeamFour.ViewModels;
+
+namespace ProjectTeamFour.Service
+{
+    public class MemberService
+    {
+        private BaseReposity<Member> _baseReposity;
+        public MemberService()
+        {
+            DbContext dbcontext = new ProjectContext();
+            _baseReposity = new BaseReposity<Member>(dbcontext);
+        }
+        public Member GetMember(int id)
+        {
+            var members= _baseReposity.GetAll();
+            Member m = members.FirstOrDefault(p => p.MemberId == id);
+            return m;
+        }
+        public Member GetMember(string account,string pwd)
+        {
+            var members = _baseReposity.GetAll();
+            Member m = members.FirstOrDefault(p => p.Account == account && p.PassWord == pwd);
+            return m;
+        }
+
+        public List<Member> GetMembers()
+        {
+            return _baseReposity.GetAll().ToList();
+        }
+        public string CreateMember(ViewMembers vm)
+        {
+            string Message = "";
+            try
+            {
+                Member m = new Member();
+                m.Account = vm.Account;
+                m.PassWord = vm.PassWord;
+                m.Name = m.Name;
+                  _baseReposity.Create(m);
+
+                Message = "新增成功";
+            }
+            catch (Exception ex)
+            {
+                Message = ex.ToString();
+            }
+            return Message;
+        }
+    }
+}
---
+++ ProjectTeamFour/ViewModels/ViewMembers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectTeamFour.ViewModels
+{
+    public class ViewMembers
+    {
+        [Required(ErrorMessage ="姓名是必填")]
+        public string Name { get; set; }
+        public string Account { get; set; }
+        [StringLength(5,ErrorMessage ="最多5個字")]
+        public string PassWord { get; set; }
+        public string PassWordConfirm { get; set; }
+        public string Position { get; set; }
+    }
+}
