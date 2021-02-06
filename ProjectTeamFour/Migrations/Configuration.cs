9d87df7cc0be09f39237df44cf8092ca3ead2f62
WangChunYu <4a037902@gmail.com>
2021-02-06 11:55:19 +0800

    productDetail

--- ProjectTeamFour/Migrations/Configuration.cs
+++ ProjectTeamFour/Migrations/Configuration.cs
@@ -25,6 +25,22 @@
                 GoalMoney = 10000000,
                 NowMoney = 289
             });
+            context.productDetails.AddOrUpdate(x => x.Id,
+                 new Models.ProductDetail
+                 {
+                     Id = 1,
+                     ProductName = "開啟與孩子的第一場程式冒險 《解碼傳說 CO·DECODE》",
+                     CreatorName = "Papacode-程式老爹",
+                     Category = "遊戲出版",
+                     FundedPeople = 828,
+                     GoalAmount = 140113,
+                     FundedAmount = 1609173,
+                     DeadLine=new DateTime(2021,3,1)
+                 },
+            new Models.ProductDetail { Id = 2, ProductName = "Cheerpod 智慧滑鼠 | 一片在手,螢幕隨控",
+                CreatorName = "cheerdots TW", Category = "科技設計", FundedPeople = 5, GoalAmount = 10000000,
+                DeadLine=new DateTime(2021,3,2)}
+            );
         }
     }
 }
