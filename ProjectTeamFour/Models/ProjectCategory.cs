b6ca45a351baaa171cba6d301fcfc876d439954c
Kane Kai <kkbbg3106@gmail.com>
2021-02-13 19:00:53 +0800

    新增 projectCategory Model

---
+++ ProjectTeamFour/Models/ProjectCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ProjectTeamFour.Models
+{
+    public class ProjectCategory
+    {
+        public int ProjectId { get; set; } //primary key
+        public string Category { get; set; }
+        public string ProjectStatus { get; set; }
+        public string ProjectCoverUrl { get; set; }
+        public string ProjectName { get; set; }
+        public decimal FundingAmount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string CreatorName { get; set; } //foreignKey
+
+        //[ForeignKey("CreatorName")]
+        //public virtual xxx CreatorName { get; set; }
+    }
+}
