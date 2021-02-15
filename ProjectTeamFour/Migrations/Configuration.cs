c8c720c96feabc88e1a7791dcb5b3d485ffe5ea5
sonia1013 <soniatoyota5941@gmail.com>
2021-02-15 11:48:53 +0800

    個人資料頁 - 新增User頁面的view及controller

--- ProjectTeamFour/Migrations/Configuration.cs
+++ ProjectTeamFour/Migrations/Configuration.cs
@@ -9,23 +9,43 @@ namespace ProjectTeamFour.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(ProjectTeamFour.Models.ProjectContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Projects.AddOrUpdate(x => x.ProjectId, new Models.Project()
+            {
+                ProjectId = 2,
+                Title = "Cheerpod 智慧滑鼠 | 一片在手,螢幕隨控",
+                MainImage = "p2.jpg",
+                CategoryName = "科技設計",
+                Pulisher = "cheerdots TW",
+                D_DateTime = new DateTime(2021, 2, 10),
+                GoalMoney = 10000000,
+                NowMoney = 289
+            });
+            context.productDetails.AddOrUpdate(x => x.Id,
+                 new Models.ProjectDetail
+                 {
+                     Id = 1,
+                     ProductName = "開啟與孩子的第一場程式冒險 《解碼傳說 CO·DECODE》",
+                     CreatorName = "Papacode-程式老爹",
+                     Category = "遊戲出版",
+                     FundedPeople = 828,
+                     GoalAmount = 140113,
+                     FundedAmount = 1609173,
+                     DeadLine=new DateTime(2021,3,1)
+                 },
+                new Models.ProjectDetail 
+                {   Id = 2, 
+                    ProductName = "Cheerpod 智慧滑鼠 | 一片在手,螢幕隨控",
+                    CreatorName = "cheerdots TW", 
+                    Category = "科技設計", 
+                    FundedPeople = 5, 
+                    GoalAmount = 10000000,
+                    DeadLine=new DateTime(2021,3,2)}
+            );
         }
     }
 }
