d1cebe295ce709d50d1e07b17b9d816729a598ec
Kane Kai <kkbbg3106@gmail.com>
2021-02-16 12:50:54 +0800

    0216上傳viewModel (project) (修改中)

---
+++ ProjectTeamFour/ViewModels/ProjectView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTeamFour.ViewModels
+{
+    public class ProjectView
+    {
+        public string ProjectMainUrl { get; set; }
+        public string Category { get; set; }
+        public string ProjectStatus { get; set; }
+        public string ProjectName { get; set; }
+        public string CreatorName { get; set; }
+        public decimal FundingAmount { get; set; }
+        public decimal AmountThreshold { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
