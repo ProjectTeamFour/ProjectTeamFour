03e4d33dc22f3115e61c666d40d0a47b830b1be5
Phil Lin <linooohon@gmail.com>
2021-02-19 19:20:28 +0800

    homeview動態產熱門, 車車完成

---
+++ ProjectTeamFour/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTeamFour.ViewModels
+{
+    public class HomeViewModel
+    {
+        public ProjectListViewModel ProjectItem { get; set; }
+        public CarCarPlanListViewModel CarCarPlanItem { get; set; }
+    }
+}
