054d68841c7ca278db1a1c5811819fa59aa03a02
YEJhen <s0984087@gmail.com>
2021-02-20 18:44:01 +0800

    新增cartViewModel&Service

---
+++ ProjectTeamFour/ViewModels/CartListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTeamFour.ViewModels
+{
+    public class CartListViewModel
+    {
+        public CartListViewModel()
+        {
+            this.CartItems = new List<CartItemViewModel>();
+        }
+
+        public  List<CartItemViewModel> CartItems{ get;set;}
+
+       
+
+        public decimal TotalAccount { get 
+            { 
+                decimal totalAccount = 0.0m;
+
+                foreach(var item in CartItems)
+                {
+                    totalAccount = totalAccount + item.Account;
+                }
+                return totalAccount;
+            } }
+    }
+}
