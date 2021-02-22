400d8945685386c94d46d15b6ebf6e6553cc28f0
Phil Lin <linooohon@gmail.com>
2021-02-22 19:15:19 +0800

    加逗號, 改layout, 刪除register, login

--- ProjectTeamFour/Assets/HeaderFooterLayout/headerfooter.js
+++ ProjectTeamFour/Assets/HeaderFooterLayout/headerfooter.js
@@ -12,4 +12,26 @@
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 
+    //$(".pc-fa-search").on("click",
+
+    //    function () {
+    //    $(".pc-input-search").blur();
+    //    $(".pc-input-search").css("border-bottom", "");
+    //},
+    //    function () {
+    //    $(".pc-input-search").focus();
+    //    $(".pc-input-search").css("border-bottom", "3px solid var(--color)");
+    //});
 });
+
+
+
+
+let faSearch = document.querySelector(".pc-fa-search");
+let inputSearch = document.querySelector(".pc-input-search");
+
+faSearch.addEventListener("click", function () {
+    inputSearch.style.borderBottom = "3px solid var(--color)";
+    inputSearch.focus();
+});
+
