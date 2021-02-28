2e07e8fa6a8cc6655a82b667a7fcc498c97aabc3
Phil Lin <linooohon@gmail.com>
2021-02-28 11:48:52 +0800

    merge

--- ProjectTeamFour/ViewModels/CarCarPlanViewModel.cs
+++ ProjectTeamFour/ViewModels/CarCarPlanViewModel.cs
@@ -11,6 +11,7 @@ namespace ProjectTeamFour.ViewModels
         public string PlanImgUrl { get; set; }
         public string Category { get; set; }
         public string ProjectName { get; set; }
+        public int ProjectId { get; set; }
         public string PlanTitle { get; set; }
         public string PlanDescription { get; set; }
         public string CreatorName { get; set; }
