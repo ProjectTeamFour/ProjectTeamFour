2683ee8730a3b3e2f4178c280345cd54c60d94f1
Kane Kai <kkbbg3106@gmail.com>
2021-03-02 19:20:41 +0800

    0302購物車畫面完成

--- ProjectTeamFour/Service/PayService.cs
+++ ProjectTeamFour/Service/PayService.cs
@@ -54,19 +54,18 @@ namespace ProjectTeamFour.Service
             {
                 var plan = _repository.GetAll<Plan>().FirstOrDefault(X => X.PlanId == item.PlanId); //資料庫id==sessionId
 
-                foreach (var items in listviewmodel.CartItems) //再撈資料庫
-                {
+               
                     var viewmodel = new CarCarPlanViewModel
-                    {
-                        PlanId= plan.PlanId,
-                        Quantity= item.Quantity,
+                    {                        
+                        PlanId = plan.PlanId,
+                        Quantity = item.Quantity,
                         PlanPrice = plan.PlanPrice,
                         PlanImgUrl = plan.PlanImgUrl,
                         PlanTitle = plan.PlanTitle,
                         ProjectId = plan.ProjectId
                     };
                     listviewmodel.CartItems.Add(viewmodel);
-                };                
+                               
             }
             return listviewmodel;
         }      
