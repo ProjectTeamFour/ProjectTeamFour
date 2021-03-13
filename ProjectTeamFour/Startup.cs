2fa5ca6c7c00a8a019b558959c9d05452133b554
TeresaYang <s1022159@gm.pu.edu.tw>
2021-03-13 17:23:12 +0800

    加入Startup檔

---
+++ ProjectTeamFour/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectTeamFour.Startup))]
+
+namespace ProjectTeamFour
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
