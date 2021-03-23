9a599d2cca95fcd2a27859e865c7e5a3cd7e04aa
sonia1013 <soniatoyota5941@gmail.com>
2021-03-23 18:58:56 +0800

    edit projectDetail/Index-Modal style

--- ProjectTeamFour/Controllers/UserInfoController.cs
+++ ProjectTeamFour/Controllers/UserInfoController.cs
@@ -15,114 +15,132 @@ using ProjectTeamFour.Helpers;
 
 namespace ProjectTeamFour.Controllers
 {
-	public class UserInfoController : Controller
-	{
-		private readonly MemberService _memberService;
-		private readonly MyProjectsService _myProjectsService;
-		private readonly CommentService _commentService;
-		private readonly BackingService _backingService;
+    public class UserInfoController : Controller
+    {
+        private readonly MemberService _memberService;
+        private readonly MyProjectsService _myProjectsService;
+        private readonly CommentService _commentService;
+        private readonly BackingService _backingService;
 
-		public UserInfoController()
-			{
-				_memberService = new MemberService();
-				_myProjectsService = new MyProjectsService();
-				_commentService = new CommentService();
-			    _backingService = new BackingService();
-			}
+        public UserInfoController()
+        {
+            _memberService = new MemberService();
+            _myProjectsService = new MyProjectsService();
+            _commentService = new CommentService();
+            _backingService = new BackingService();
+        }
 
-		// GET: PersonInfo
-		//[CustomAuthorize(flagNum = 1)]
-		public ActionResult Member(int Id)	//公開的個人資料頁面
-		{
-			var model = (MemberViewModel)Session["Member"];
-			if (model != null)
-			{
-				var memberInfo = _memberService.GetMember(m => m.MemberId == Id);
+        // GET: PersonInfo
+        //[CustomAuthorize(flagNum = 1)]
+        public ActionResult Member(int Id)  //公開的個人資料頁面
+        {
+            var model = (MemberViewModel)Session["Member"];
+            if (model != null)
+            {
+                var memberInfo = _memberService.GetMember(m => m.MemberId == Id);
 
 
-				//根據專案的提交與審核狀態進行分類
-				model.MyProjects  = _myProjectsService.GetProjectsbyMemberId(model.MemberId);
-				
-				//根據會員id抓取會員購買紀錄
-			    model.Records = _backingService.QueryOrder(model.MemberId);
+                //根據專案的提交與審核狀態進行分類
+                model.MyProjects = _myProjectsService.GetProjectsbyMemberId(model.MemberId);
 
-				return model != default(ViewModels.MemberViewModel) ? View(model) : View();
-			}
+                //根據會員id抓取會員購買紀錄
+                model.Records = _backingService.QueryOrder(model.MemberId);
+
+                return model != default(ViewModels.MemberViewModel) ? View(model) : View();
+            }
             else
             {
-				return RedirectToAction("Login", "Member");
+                return RedirectToAction("Login", "Member");
             }
-		}
+        }
+        public ActionResult Users(int Id)
+        {
 
+            var memberInfo = _memberService.GetMember(m => m.MemberId == Id);
+            if (memberInfo != null)
+            {
+                //根據專案的提交與審核狀態進行分類
+                memberInfo.MyProjects = _myProjectsService.GetProjectsbyMemberId(memberInfo.MemberId);
 
-		//public ActionResult Sponser() //贊助紀錄
-		//{
-		//	var model = (MemberViewModel)Session["Member"];
-		//	if (model != null)
-		//	{
+                //根據會員id抓取會員購買紀錄
+                memberInfo.Records = _backingService.QueryOrder(memberInfo.MemberId);
 
-		//		//根據會員id抓取會員購買紀錄
-		//		model.Records = _backingService.QueryOrder(model.MemberId);				
-		//	}
-		//	return View();
-			
-		//}
+                return View();
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
 
-		//public ActionResult Myprojects()	//專案提交紀錄
-		//{
-		//	var model = (MemberViewModel)Session["Member"];
-		//	if (model != null)
-		//	{
-		//		//根據專案的提交與審核狀態進行分類
-		//		//model.MyProjects  = _myProjectsService.GetProjectsbyMemberId(model.MemberId);				
-		//		return View(model);
-		//	}
-  //          else
-  //          {
-		//		return RedirectToAction("Login", "Member");
-		//	}
-		//}
+        //public ActionResult Sponser() //贊助紀錄
+        //{
+        //	var model = (MemberViewModel)Session["Member"];
+        //	if (model != null)
+        //	{
 
-		public ActionResult Message()		//聯絡訊息
-		{
-			var model = (MemberViewModel)Session["Member"];
-			return View(model);
-		}
+        //		//根據會員id抓取會員購買紀錄
+        //		model.Records = _backingService.QueryOrder(model.MemberId);				
+        //	}
+        //	return View();
 
-		public ActionResult Activity()	//最新通知(可以連動贊助或購買商品後收發訂單狀態的email功能)
-		{
-			var model = (MemberViewModel)Session["Member"];
-			return View(model);
-		}
+        //}
 
-		public ActionResult Edit()	//修改個人資料
-		{
-			var model = (MemberViewModel)Session["Member"];
+        //public ActionResult Myprojects()	//專案提交紀錄
+        //{
+        //	var model = (MemberViewModel)Session["Member"];
+        //	if (model != null)
+        //	{
+        //		//根據專案的提交與審核狀態進行分類
+        //		//model.MyProjects  = _myProjectsService.GetProjectsbyMemberId(model.MemberId);				
+        //		return View(model);
+        //	}
+        //          else
+        //          {
+        //		return RedirectToAction("Login", "Member");
+        //	}
+        //}
 
-			MemberViewModel memberVM = new MemberViewModel();
+        //public ActionResult Message()		//聯絡訊息
+        //{
+        //	var model = (MemberViewModel)Session["Member"];
+        //	return View(model);
+        //}
 
-			var memberInfo = _memberService.GetMember(m => m.MemberId == model.MemberId);
-			//memberInfo = memberVM;
-			//return RedirectToAction("Index");
-			return memberInfo != default(ViewModels.MemberViewModel) ? View(memberInfo) : View();
-		}
+        //public ActionResult Activity()	//最新通知(可以連動贊助或購買商品後收發訂單狀態的email功能)
+        //{
+        //	var model = (MemberViewModel)Session["Member"];
+        //	return View(model);
+        //}
 
-		public ActionResult Account()	//修改密碼以及紀錄第三方登入的會員資料
-		{
-			var model = (MemberViewModel)Session["Member"];
+        //public ActionResult Edit()	//修改個人資料
+        //{
+        //	var model = (MemberViewModel)Session["Member"];
 
-			MemberViewModel memberVM = new MemberViewModel();
+        //	MemberViewModel memberVM = new MemberViewModel();
 
-			var memberInfo = _memberService.GetMember(m => m.MemberId == model.MemberId);
+        //	var memberInfo = _memberService.GetMember(m => m.MemberId == model.MemberId);
+        //	//memberInfo = memberVM;
+        //	//return RedirectToAction("Index");
+        //	return memberInfo != default(ViewModels.MemberViewModel) ? View(memberInfo) : View();
+        //}
 
-			//return RedirectToAction("Index");
-			return memberInfo != default(ViewModels.MemberViewModel) ? View(memberInfo) : View();
-			//return View();
-		}
+        //public ActionResult Account()	//修改密碼以及紀錄第三方登入的會員資料
+        //{
+        //	var model = (MemberViewModel)Session["Member"];
 
-		//public ActionResult Notifaction()	//通知設定
-		//{
-		//	return View();
-		//}
-	}
+        //	MemberViewModel memberVM = new MemberViewModel();
+
+        //	var memberInfo = _memberService.GetMember(m => m.MemberId == model.MemberId);
+
+        //	//return RedirectToAction("Index");
+        //	return memberInfo != default(ViewModels.MemberViewModel) ? View(memberInfo) : View();
+        //	//return View();
+        //}
+
+        //public ActionResult Notifaction()	//通知設定
+        //{
+        //	return View();
+        //}
+    }
 }
