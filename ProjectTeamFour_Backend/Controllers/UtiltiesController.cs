28f75092aa0cbc35fd13486ec620df1765ac9146
Kane Kai <kkbbg3106@gmail.com>
2021-03-26 16:59:47 +0800

    0326後台建置view

---
+++ ProjectTeamFour_Backend/Controllers/UtiltiesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectTeamFour_Backend.Controllers
+{
+    public class UtiltiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Colors()
+        {
+            return View();
+        }
+        public IActionResult Borders()
+        {
+            return View();
+        }
+        public IActionResult Animations()
+        {
+            return View();
+        }
+        public IActionResult Other()
+        {
+            return View();
+        }
+
+    }
+}
