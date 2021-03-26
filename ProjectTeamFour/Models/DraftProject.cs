f71c83e74acd9e25f0b15610c774cb97ef2682f6
Phil Lin <linooohon@gmail.com>
2021-03-27 04:20:57 +0800

    migration draft project plan model

--- ProjectTeamFour/Models/DraftProject.cs
+++ ProjectTeamFour/Models/DraftProject.cs
@@ -7,33 +7,37 @@ namespace ProjectTeamFour.Models
 {
     public class DraftProject
     {
+
         public int DraftProjectId { get; set; }
         public string DraftProjectName { get; set; }
-        public decimal FundingAmount { get; set; }
-        public string Category { get; set; }
-        
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-
-        //public double dateLine { get; set; }
-        public int MemberId { get; set; }
-        public int Fundedpeople { get; set; }
-        public string DraftProjectDescription { get; set; }
-        public string DraftProjectImgUrl { get; set; }
+        public decimal DraftAmountThreshold { get; set; }
+        public string DraftCategory { get; set; }
+        public DateTime DraftStartDate { get; set; }
+        public DateTime DraftEndDate { get; set; }
         public string DraftProjectVideoUrl { get; set; }
+        public string DraftProjectMainUrl { get; set; }
+        public string DraftProjectCoverUrl { get; set; }
+        public string DraftMemberConEmail { get; set; }
+        public string DraftMemberPhone { get; set; }
+        public string DraftProfileImgUrl { get; set; }
+        public string DraftCreatorName { get; set; }
+        public string DraftAboutMe { get; set; }
+        public string DraftMemberWebsite { get; set; }
+        public string DraftProjectImgUrl { get; set; }
+        public string DraftProjectPrincipal { get; set; }
+        public string DraftIdentityNumber { get; set; }
         public string DraftProject_Question { get; set; }
         public string DraftProject_Answer { get; set; }
+        public DateTime DraftCreatedDate { get; set; }
+        public DateTime DraftSubmittedDate { get; set; }
+        public DateTime DraftLastEditTime { get; set; }
+        public decimal DraftFundingAmount { get; set; }
+        public int DraftFundedpeople { get; set; }
+        public List<DraftPlan> DraftPlanObject { get; set; }
+        public int ApprovingStatus { get; set; }
+        public string ProjectStatus { get; set; }
 
 
-        //可以動態取得看是否拿掉plan.Count(p=>p.projectid==?)......................... 
-        public int DraftProjectPlansCount { get; set; }
-        public string DraftProjectCoverUrl { get; set; }
-        public decimal AmountThreshold { get; set; }
-        public string CreatorName { get; set; }
-        public string DraftProjectMainUrl { get; set; }
-        public string DraftProjectPrincipal { get; set; }
-        public string IdentityNumber { get; set; }
 
-        public virtual Member Member { get; set; }
     }
 }
