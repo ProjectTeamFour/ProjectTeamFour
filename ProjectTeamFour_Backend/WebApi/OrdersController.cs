1500a770576e391c1c1bbcfc0b0f19923b293870
Kane Kai <kkbbg3106@gmail.com>
2021-03-31 11:20:11 +0800

    0331 orderapi測試正常

--- ProjectTeamFour_Backend/WebApi/OrdersController.cs
+++ ProjectTeamFour_Backend/WebApi/OrdersController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using ProjectTeamFour_Backend.Context;
 using ProjectTeamFour_Backend.Models;
 using ProjectTeamFour_Backend.Services;
@@ -16,21 +18,19 @@ namespace ProjectTeamFour_Backend.WebApi
     public class OrdersController : ControllerBase
     {
         private readonly LabContext _context;
-        private readonly OrderService _orderservice;
+        private readonly ILogger<LabContext> _logger;
 
-        public OrdersController(LabContext context)
+        public OrdersController(LabContext context, ILogger<LabContext> logger)
         {
             _context = context;
+            _logger = logger;
         }
-
-        //public class Order 
-
-
 
         // GET: api/Orders
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
         {
+            _logger.LogWarning(2001, DateTime.Now.ToLongTimeString() + "Products控制器Get方法被呼叫, by" + UriHelper.GetDisplayUrl(Request)); //確認前後參數是否一致
             return await _context.Orders.ToListAsync();
         }
 
@@ -53,6 +53,7 @@ namespace ProjectTeamFour_Backend.WebApi
         [HttpPut("{id}")]
         public async Task<IActionResult> PutOrder(int id, Order order)
         {
+            _logger.LogWarning(2001, DateTime.Now.ToLongTimeString() + "Products控制器PUT方法被呼叫-傳入的資料為:" + System.Text.Json.JsonSerializer.Serialize(order));
             if (id != order.OrderId)
             {
                 return BadRequest();
@@ -82,8 +83,10 @@ namespace ProjectTeamFour_Backend.WebApi
         // POST: api/Orders
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Order>> PostOrder(Order order)
+        public async Task<ActionResult<Order>> PostOrder([FromBody]Order order)
         {
+            _logger.LogWarning(2001, DateTime.Now.ToLongTimeString() + "Products控制器Post方法被呼叫-傳入的資料為:" + System.Text.Json.JsonSerializer.Serialize(order));
+
             _context.Orders.Add(order);
             await _context.SaveChangesAsync();
 
@@ -94,6 +97,7 @@ namespace ProjectTeamFour_Backend.WebApi
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteOrder(int id)
         {
+            _logger.LogWarning(2001, DateTime.Now.ToLongTimeString() + "Products控制器Delete方法被呼叫, by" + UriHelper.GetDisplayUrl(Request)); //確認前後端參數是否一致
             var order = await _context.Orders.FindAsync(id);
             if (order == null)
             {
