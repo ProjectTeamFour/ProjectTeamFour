7dfe596592aed59fd6c8f31fdf1c466797bbdc2f
Shrank Kao <a23057475@gmail.com>
2021-04-06 21:15:53 +0800

    新增通知CRUD

--- ProjectTeamFour_Backend/wwwroot/js/Announcement.js
+++ ProjectTeamFour_Backend/wwwroot/js/Announcement.js
@@ -9,7 +9,7 @@
         items: [],
         fields: [
             { key: 'announcementId', label: '編號', sortable: true },
-            { key: 'title', label: '通知標題', class: '' },
+            { key: 'title', label: '通知標題', class: 'test' },
             { key: 'createTime', label: '發布時間', class: '' },
             { key: 'createUser', label: '發布人', class: '' },
             { key: 'editTime', label: '編輯時間', class: '' },
@@ -75,14 +75,20 @@
         },
         info(item, index, button) {
             this.infoModal.title = `編號: ${JSON.stringify(item.announcementId, null, 2)}`
-            this.infoModal.content = `編號:${JSON.stringify(item.announcementId, null, 2)}\n\n
-通知標題:${JSON.stringify(item.title, null, 2)}\n\n
-通知內容:${JSON.stringify(item.content, null, 2)}\n\n
-發布時間:${JSON.stringify(item.createTime, null, 2)}\n\n
-發布人:${JSON.stringify(item.createUser, null, 2)}\n\n
-編輯時間:${JSON.stringify(item.editTime, null, 2)}\n\n
-編輯人:${JSON.stringify(item.editUser, null, 2)}\n\n`
+            this.infoModal.id = `編號:${JSON.stringify(item.announcementId, null, 2)}\n\n`
+            this.infoModal.content = `\n${JSON.stringify(item.content, null, 0)}\n\n`
+            this.infoModal.createTime = `\n發布時間:${JSON.stringify(item.createTime, null, 2)}\n\n`
+            this.infoModal.createUser = `發布人:${JSON.stringify(item.createUser, null, 2)}\n\n`
+            this.infoModal.editTime = `編輯時間:${JSON.stringify(item.editTime, null, 2)}\n\n`
+            this.infoModal.editUser = `編輯人:${JSON.stringify(item.editUser, null, 2)}\n\n`
             this.$root.$emit('bv::show::modal', this.infoModal.id, button)
+        },
+        edit(item, index, button) {
+            console.log(this.item);
+        },
+        remove(item, index, button) {
+            console.log("123");
+            axios.delete(`/Api/Announcements/DeleteAnnouncement?announcementId=${this.index}`);
         },
         resetInfoModal() {
             this.infoModal.title = ''
