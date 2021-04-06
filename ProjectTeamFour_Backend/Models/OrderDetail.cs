f6b2aee6f3b9988c856512e2c19e21f4d7bbe0a1
YEJhen <s0984087@gmail.com>
2021-04-06 16:06:36 +0800

    新增欄位

--- ProjectTeamFour_Backend/Models/OrderDetail.cs
+++ ProjectTeamFour_Backend/Models/OrderDetail.cs
@@ -17,7 +17,6 @@ namespace ProjectTeamFour_Backend.Models
         public int ProjectId { get; set; }
         public string OrderPlanImgUrl { get; set; }
         public string Condition { get; set; }
-        public DateTime PlanShipDate { get; set; }
 
         public virtual Order Order { get; set; }
         public virtual Plan Plan { get; set; }
