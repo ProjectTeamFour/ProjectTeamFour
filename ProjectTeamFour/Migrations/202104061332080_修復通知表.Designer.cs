a57921963383347b7d96175b64ad2944b96b5732
Shrank Kao <a23057475@gmail.com>
2021-04-06 21:32:36 +0800

    修復通知表

--- ProjectTeamFour/Migrations/202104061332080_修復通知表.Designer.cs
+++ ProjectTeamFour/Migrations/202104061332080_修復通知表.Designer.cs
@@ -13,7 +13,7 @@ namespace ProjectTeamFour.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "202104061001587_修復通知表"; }
+            get { return "202104061332080_修復通知表"; }
         }
         
         string IMigrationMetadata.Source
