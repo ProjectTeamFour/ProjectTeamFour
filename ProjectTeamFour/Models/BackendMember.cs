74dc42975b7820d203b5dccfadb160e9ba0ae725
YEJhen <s0984087@gmail.com>
2021-04-09 13:44:55 +0800

    新增前台Context Db<set>

--- ProjectTeamFour/Models/BackendMember.cs
+++ ProjectTeamFour/Models/BackendMember.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace ProjectTeamFour.Models
 {
     public class Backendmember
     {
+        
         public int MemberId { get; set; }
         /// <summary>
         /// 後臺會員真實姓名
