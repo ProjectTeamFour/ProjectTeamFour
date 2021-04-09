8e507047f40c9ae8662303d4bb76d829cf85d8c3
Kane Kai <kkbbg3106@gmail.com>
2021-04-09 13:56:25 +0800

    0409 order view更新 layout修改

--- ProjectTeamFour/Service/ProjectDetailEntityService.cs
+++ ProjectTeamFour/Service/ProjectDetailEntityService.cs
@@ -151,6 +151,7 @@ namespace ProjectTeamFour.Service
 
             DateTime today = DateTime.Now;
             double dateLine = Convert.ToInt32(new TimeSpan(item.EndDate.Ticks - today.Ticks).TotalDays);
+            var o = _repository.GetAll<Order>().Where(x => x.MemberId == item.MemberId).Select(x => x).ToList();
 
             if (dateLine <= 0 && item.FundingAmount > item.AmountThreshold)
             {
@@ -159,6 +160,10 @@ namespace ProjectTeamFour.Service
             else if (dateLine <= 0 && item.FundingAmount < item.AmountThreshold)
             {
                 item.ProjectStatus = "結束且失敗";
+                foreach(var i in o)
+                {
+                    i.condition = "需退款";
+                }
             }
             else if (dateLine > 0 && item.FundingAmount > item.AmountThreshold)
             {
